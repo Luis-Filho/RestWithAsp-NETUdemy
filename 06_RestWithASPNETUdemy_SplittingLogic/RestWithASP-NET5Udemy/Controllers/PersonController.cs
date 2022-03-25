4231efe4b835832f589a84275482370bccd60bec
Luis Costa <luis.filho@outlook.com.br>
2022-03-25 00:12:20 -0300

    Adicionado projeto para novo modulo.

---
+++ 06_RestWithASPNETUdemy_SplittingLogic/RestWithASP-NET5Udemy/Controllers/PersonController.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using RestWithASP_NET5Udemy.Model;
+using RestWithASP_NET5Udemy.Services;
+
+namespace RestWithASP_NET5Udemy.Controllers
+{
+    [ApiVersion("1")]
+    [ApiController]
+    [Route("api/[controller]/v{version:apiVersion}")]
+    public class PersonController : ControllerBase
+    {
+        private readonly ILogger<PersonController> _logger;
+        private IPersonService _personService;
+
+        public PersonController(ILogger<PersonController> logger, IPersonService personService)
+        {
+            _logger = logger;
+            _personService = personService;
+        }
+
+        [HttpGet]
+        public IActionResult Get() =>
+            Ok(_personService.FindAll());
+
+        [HttpGet("{id:long}")]
+        public IActionResult Get(long id)
+        {
+            var person = _personService.FindById(id);
+
+            if (person == null) return NotFound();
+            return Ok(person);
+        }
+
+        [HttpPost]
+        public IActionResult Post([FromBody] Person person)
+        {
+            if (person == null) return BadRequest();
+            return Ok(_personService.Create(person));
+        }
+
+        [HttpPut]
+        public IActionResult Put([FromBody] Person person)
+        {
+            if (person == null) return BadRequest();
+            return Ok(_personService.Update(person));
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(long id)
+        {
+            _personService.Delete(id);
+            return NoContent();
+        }
+    }
+}
---
+++ 06_RestWithASPNETUdemy_SplittingLogic/RestWithASP-NET5Udemy/Services/Implementation/PersonServiceImplementation.cs
@@ -0,0 +1,77 @@
+﻿using RestWithASP_NET5Udemy.Model;
+using RestWithASP_NET5Udemy.Model.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+
+namespace RestWithASP_NET5Udemy.Services.Implementation
+{
+    public class PersonServiceImplementation : IPersonService
+    {
+        private MySQLContext _context;
+        public PersonServiceImplementation(MySQLContext context) =>
+            _context = context;
+
+        public Person Create(Person person)
+        {
+            try
+            {
+                _context.Add(person);
+                _context.SaveChanges();
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
+            return person;
+        }
+
+
+        public void Delete(long id)
+        {
+            var result = _context.Persons.SingleOrDefault(x => x.Id == id);
+
+            if (result != null)
+                try
+                {
+                    _context.Persons.Remove(result);
+                    _context.SaveChanges();
+                }
+                catch (Exception)
+                {
+                    throw;
+                }
+        }
+
+        public List<Person> FindAll() =>
+            _context.Persons.ToList();
+
+        public Person FindById(long id) =>
+            _context.Persons.SingleOrDefault(x => x.Id == id);
+
+        public Person Update(Person person)
+        {
+            if (!Exists(person.Id)) return new Person();
+
+            var result = _context.Persons.SingleOrDefault(x => x.Id == person.Id);
+
+            if (result != null)
+                try
+                {
+                    _context.Entry(result).CurrentValues.SetValues(person);
+                    _context.SaveChanges();
+                }
+                catch (Exception)
+                {
+                    throw;
+                }
+            return person;
+        }
+
+        private bool Exists(long id) =>
+            _context.Persons.Any(x => x.Id == id);
+    }
+}
