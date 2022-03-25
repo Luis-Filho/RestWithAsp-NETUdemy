4dd45a45988e4cb6460cb0336e412beb68f1d300
Luis Costa <luis.filho@outlook.com.br>
2022-03-25 15:52:43 -0300

    Adicionado projeto base para implementação do repositoryGeneric

---
+++ 08_RestWithASPNETUdemy_WorkingWithGenericRepository/RestWithASP-NET5Udemy/Business/Implementation/PersonBusinessImplementation.cs
@@ -0,0 +1,32 @@
+﻿using RestWithASP_NET5Udemy.Model;
+using RestWithASP_NET5Udemy.Model.Context;
+using RestWithASP_NET5Udemy.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace RestWithASP_NET5Udemy.Business.Implementation
+{
+    public class PersonBusinessImplementation : IPersonBusiness
+    {
+        private readonly IPersonRepository _repository;
+        public PersonBusinessImplementation(IPersonRepository repository) =>
+            _repository = repository;
+
+        public List<Person> FindAll() =>
+            _repository.FindAll();
+
+        public Person FindById(long id) =>
+            _repository.FindById(id);
+
+        public Person Create(Person person) =>
+            _repository.Create(person);
+
+        public void Delete(long id) =>
+            _repository.Delete(id);
+
+
+        public Person Update(Person person) =>
+            _repository.Update(person);
+    }
+}
